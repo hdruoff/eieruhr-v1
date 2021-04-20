dd82360c67dfb1cf0cf08ecf58e6dae8690ee622
ha-dida <hdruoff@gmx.de>
2021-04-20 21:35:44 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,62 @@
+input.onButtonPressed(Button.A, function () {
+    zeit += -10
+    if (zeit < 1) {
+        zeit = 10
+    }
+})
+input.onGesture(Gesture.Shake, function () {
+    if (einstellmodus) {
+        einstellmodus = false
+        einstellmodus = input.runningTime()
+    } else {
+        start = true
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    zeit += 10
+})
+let restzeit = 0
+let vergangene_sek = 0
+let einstellmodus = false
+let zeit = 0
+let start = false
+start = true
+basic.forever(function () {
+    if (!(einstellmodus)) {
+        vergangene_sek = input.runningTime() - start / 1000
+        restzeit = zeit - vergangene_sek
+    }
+    if (restzeit <= 0) {
+        while (!(einstellmodus)) {
+            basic.showLeds(`
+                # . . . #
+                . # # # .
+                . . # . .
+                . # . # .
+                . . # . .
+                `)
+            for (let index = 0; index < 4; index++) {
+                music.playTone(392, music.beat(BeatFraction.Sixteenth))
+                music.rest(music.beat(BeatFraction.Sixteenth))
+                basic.showLeds(`
+                    . # . # .
+                    . # # # .
+                    . . # . .
+                    . # . # .
+                    . . # . .
+                    `)
+                for (let index = 0; index < 4; index++) {
+                    music.playTone(523, music.beat(BeatFraction.Sixteenth))
+                    music.rest(music.beat(BeatFraction.Sixteenth))
+                }
+            }
+        }
+    } else if (restzeit >= 0) {
+        basic.showNumber(restzeit)
+    } else {
+        vergangene_sek = 10
+        while (einstellmodus) {
+            basic.showNumber(zeit)
+        }
+    }
+})
